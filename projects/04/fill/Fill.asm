8da3aa40bb5e56a999cccc28c3781e6d23aef2a9
qwyng <ikusawasi@gmail.com>
2020-01-21 23:44:01 +0900

    create Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,63 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@i
+M=0
+
+(WHITE_LOOP)
+    @KBD
+    D=M
+    @RESET_BLACK
+    D;JGT
+
+    @i
+    D=M
+    @8192 // 16ビット幅で横は512ビット縦は256
+    D=D-A
+    @RESET_WHITE
+    D;JGE
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+    @i
+    M=D+1
+    @WHITE_LOOP
+    0; JMP
+
+(RESET_WHITE)
+    @i
+    M=0
+    @WHITE_LOOP
+    0;JMP
+
+(BLACK_LOOP)
+    @KBD
+    D=M
+    @RESET_WHITE
+    D;JEQ
+
+    @i
+    D=M
+    @8192
+    D=D-A
+    @RESET_BLACK
+    D;JGE
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=1 //ここなんで-1にさせると全ビット黒くなるのかわからん
+    @i
+    M=D+1
+    @BLACK_LOOP
+    0;JMP
+
+(RESET_BLACK)
+    @i
+    M=0
+    @BLACK_LOOP
+    0;JMP
