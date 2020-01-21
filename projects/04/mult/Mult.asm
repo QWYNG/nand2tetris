a57562a045696c62a2beaf333b754739b80b1e29
qwyng <ikusawasi@gmail.com>
2020-01-21 20:16:54 +0900

    create Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@i
+M=0
+@sum
+M=0
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=M-D
+    @END
+    D;JEQ
+    @R0
+    D=M
+    @sum
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @sum
+    D=M
+    @R2
+    M=D
+    @END
+    0;JMP
